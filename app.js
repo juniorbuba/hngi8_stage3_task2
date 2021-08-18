cb49470ea15b7d62a9e9914c9a286ce6391f13f1
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2021-08-19 00:39:19 +0100

    chore: committed working files, remain to test in prod

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const path = require('path')
+const nodemailer = require('nodemailer')
+const multiparty = require('multiparty')
+require('dotenv').config()
+
+const app = express()
+
+const pathToViews = path.join(__dirname, 'src/assets/views')
+const pathToPublic = path.join(__dirname, 'public/css')
+const pathToJS = path.join(__dirname, 'public/js')
+
+const port = process.env.PORT || 6500
+
+app.set('views', pathToViews)
+app.use(express.static(pathToPublic))
+app.use(express.static(pathToJS))
+app.use(express.static(pathToViews))
+
+app.get('/', (req, res) => res.sendFile(pathToViews+ '/resume.html'))
+
+app.get('/contact-me', (req, res) => res.sendFile(pathToViews, '/contact.html'))
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 25,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+transporter.verify( (error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+  app.post("/send", (req, res) => {
+   
+    console.log('mailsent')
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+      console.log(fields);
+      Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+      });
+  
+      const mail = {
+        from: data.name,
+        to: process.env.EMAIL,
+        subject: data.subject,
+        text: `${data.name} <${data.email}> \n${data.message}`,
+      };
+  
+      transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Something went wrong.");
+        } else {
+          res.status(200).send("Email successfully sent to recipient!");
+        }
+      });
+    });
+  });
+  
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
---
+++ public/js/mail.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("contact-form"); 
+
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let mail = new FormData(form);
+  sendMail(mail);
+})
+
+const sendMail = (mail) => {
+    
+    fetch("/send", {
+      method: "post",
+      body: mail,
+  
+    }).then((response) => {
+      return response.json();
+    });
+  };
+  
